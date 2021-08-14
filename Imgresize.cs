5c86cc62f419e011048061a245957216c7b325d0
Sovietball1.0 <77866829+Sovietball1922@users.noreply.github.com>
2021-08-15 00:18:16 +0900

    add partial arguments

--- Imgresize.cs
+++ Imgresize.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
 
 namespace ImageResizer
 {
@@ -10,10 +12,14 @@ namespace ImageResizer
     {
         static void Main(string[] args)
         {
-            string date = DateTime.Now.ToString();
-            Console.WriteLine($"today: {date}");
-            Console.ReadKey();
         }
+
+
+    }
+
+    class OptionAnalyzer
+    {
+
     }
 
     class JsonSerializer
@@ -21,13 +27,34 @@ namespace ImageResizer
 
     }
 
-    class ArgumentAnalyzer
+    class Options
     {
+        [Option('i', "input", HelpText = "入力先のファイル名")]
+        public string InputPath { get; set; }
 
-    }
+        [Option('o', "output", HelpText = "出力先のファイル名")]
+        public string OutputPath { get; set; }
 
-    class Params
-    {
+        [Option('r', "ratio", HelpText = "画像の拡大/縮小率")]
+        public int Ratio { get; set; }
 
+        [Option('c', "clipboad", HelpText = "クリップボードから読み込む")]
+        public bool UseClipboad { get; set; }
+
+        [Option("overwrite", HelpText = "元のファイルを上書きする")]
+        public bool Overwrite { get; set; }
+
+        public string ShowUsage()
+        {
+            var help = new HelpText();
+
+            help.AddDashesToOption = true;
+
+            help.AddPreOptionsLine("構文:  imresize [options] \n");
+            help.AddPreOptionsLine("オプション");
+            help.AddOptions<Options>(this);
+
+            return help;
+        }
     }
 }
