d4acc595a1f878091e5fe2ff9c66d227cf17ed45
Sovietball1.0 <77866829+Sovietball1922@users.noreply.github.com>
2021-08-17 00:51:31 +0900

    update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,1 +1,41 @@
 @echo off
+
+set PATH=%PATH%;%~dp0
+set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+
+title Compiling...
+
+csc.exe ^
+    /r:mscorlib.dll ^
+    /r:netstandard.dll ^
+    /r:System.dll ^
+    /r:System.IO.dll ^
+    /r:System.Linq.dll ^
+    /r:System.Collections.dll ^
+    /r:System.Diagnostics.Process.dll ^
+    /r:System.XML.dll ^
+    /r:System.Xml.Linq.dll ^
+    /r:System.Text.Encoding.dll ^
+    /r:System.Runtime.dll ^
+    %1
+
+if errorlevel 1 goto ERROR
+
+echo  ---  Compile Succeeded  ---
+title Succeeded
+color 2F
+echo.
+pause
+start %~dp0\
+color
+exit
+
+:ERROR
+echo.
+echo  ---  Compile Failed  ---
+title Failed
+color 4F
+echo.
+pause
+color
+exit
