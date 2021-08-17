83f6f1dd403cb0c7a72461e21211fb8957c11ccc
Sovietball1.0 <77866829+Sovietball1922@users.noreply.github.com>
2021-08-17 11:00:49 +0900

    update compile script

--- compile.bat
+++ compile.bat
@@ -2,22 +2,15 @@
 
 set PATH=%PATH%;%~dp0
 set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+set TARGET_0=imgresize
 
 title Compiling...
 
-csc.exe ^
-    /r:mscorlib.dll ^
-    /r:netstandard.dll ^
-    /r:System.dll ^
-    /r:System.IO.dll ^
-    /r:System.Linq.dll ^
-    /r:System.Collections.dll ^
-    /r:System.Diagnostics.Process.dll ^
-    /r:System.XML.dll ^
-    /r:System.Xml.Linq.dll ^
-    /r:System.Text.Encoding.dll ^
-    /r:System.Runtime.dll ^
-    %1
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe ^
+    /r:mscorlib.dll                                     ^
+    /r:netstandard.dll                                  ^
+    /r:System.dll                                       ^
+    %~dp0\%TARGET%.cs
 
 if errorlevel 1 goto ERROR
 
@@ -26,7 +19,7 @@ title Succeeded
 color 2F
 echo.
 pause
-start %~dp0\
+start %~dp0\%TARGET_1%.exe
 color
 exit
 
